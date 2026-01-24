dfeba42882b18d2f4c256d014dd00773edf41f6d
artttenorio <arthurtenorio0101@gmail.com>
2026-01-24 17:15:58 -0300

    chore: add Google Tag Manager

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-TLZ5PW2R",
+};
+
+TagManager.initialize(tagManagerArgs);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
